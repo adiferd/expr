b52e5177fc6f7b5e97cf2eac1b89197ddb582d28
Adi Ferdian <af@aichat.co>
2018-04-04 10:56:05 +0700

    Initiate apidoc to generate rest api documentation

--- app/controllers/Hello.js
+++ app/controllers/Hello.js
@@ -2,10 +2,41 @@
 const Formatter = require(`${process.env.PWD}/app/helpers/Formatter`);
 
 class Hello {
+  /**
+   * @api {get} /user/:id Request User information
+   * @apiName GetUser
+   * @apiGroup User
+   *
+   * @apiParam {Number} id Users unique ID.
+   *
+   * @apiSuccess {String} firstname Firstname of the User.
+   * @apiSuccess {String} lastname  Lastname of the User.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "firstname": "John",
+   *       "lastname": "Doe"
+   *     }
+   *
+   * @apiError UserNotFound The id of the User was not found.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {
+   *       "error": "UserNotFound"
+   *     }
+   */
   static async sayHello(req, res, next) {
-    let response = Formatter.toSingleResponse({hello: 'world'}, 'Say hello')
-    res.send(response)
-    next()
+      try {
+          let response = Formatter.toSingleResponse({hello: 'world'}, 'Say hello')
+          res.send(response)
+      } catch (e) {
+          let response = Formatter.toSingleResponse({hello: e}, 'Error occured')
+          res.send(response)
+      } finally {
+          next()
+      }
   }
 }
 
