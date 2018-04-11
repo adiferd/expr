ba95bf5e258f44012408f78929dc9e1114d6ac63
Adi Ferdian <af@aichat.co>
2018-04-11 14:50:43 +0700

    [AUTH] Create Authentication
    Authentication done using JsonWebToken(JWT) and the client will get access token to be used on request to resource endpoint

---
+++ app/controllers/Auth.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Formatter = require(`${process.env.PWD}/app/helpers/Formatter`);
+const ConfigHelper = require(`${process.env.PWD}/app/helpers/Config`);
+const AuthService = require(`${process.env.PWD}/app/services/AuthService`);
+
+class Auth {
+
+  static async doAuthentication(req, res, next) {
+    try {
+      let body = req.body;
+      // console.log(body);
+      const auth =  await AuthService.doAuthentication(
+                      req.body.app_name,
+                      req.body.app_secret
+                    );
+      let response = Formatter.toSingleResponse(auth, 'Successfully authenticated');
+      res.status(200).send(response);
+    } catch (e) {
+      let response = Formatter.toSingleResponse({error:e}, 'Error occured');
+      res.status(400).send(response);
+    } finally {
+      next()
+    }
+  }
+
+  static async getAccessToken(req, res, next) {
+    try {
+        let body = req.body
+      if(body.app_id === `${ConfigHelper.get('client').app_id}` && body.app){
+        res.status(200).send()
+      }
+    } catch (e) {
+
+    } finally {
+      next()
+    }
+  }
+}
+
+module.exports = Auth;
---
+++ app/services/AuthService.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ConfigHelper = require(`${process.env.PWD}/app/helpers/Config`);
+const Hash = require(`${process.env.PWD}/app/helpers/Hash`);
+
+class AuthService {
+  static async doAuthentication(name, secret) {
+      if (name === ConfigHelper.get('client').app_name &&
+        secret === ConfigHelper.get('client').app_secret) {
+          let signedObject = {
+            app_name : ConfigHelper.get('client').app_name,
+            app_id : ConfigHelper.get('client').app_id,
+            app_secret : ConfigHelper.get('client').app_secret
+          }
+
+          signedObject = await Hash.jwtSign(signedObject, ConfigHelper.get('client').access_token);
+          return {
+            appData: {
+              appname: name,
+              appsecret: secret
+            },
+            token: signedObject
+          };
+      }
+      else {
+        throw {
+				        message: 'Invalid Credential',
+				        status: 400
+			        }
+      }
+  }
+}
+
+module.exports = AuthService;
