34417bafc80e5914a52464dd51198aa0cfbdbd20
firyalff <firyal.fakhril@gmail.com>
2018-04-28 14:38:45 +0700

    bugfix route group middleware init

--- app/kernels/Routing.js
+++ app/kernels/Routing.js
@@ -1,27 +1,29 @@
-'use strict';
-const Controllers = require(`${process.env.PWD}/app/controllers`);
-const Middlewares = require(`${process.env.PWD}/app/middlewares`);
-const Routes = require(`${process.env.PWD}/app/routes`);
+"use strict";
+const Controllers= require(`${process.env.PWD}/app/controllers`);
+const Middlewares= require(`${process.env.PWD}/app/middlewares`);
+const Routes= require(`${process.env.PWD}/app/routes`);
 
-const Utils = require(`${process.env.PWD}/app/helpers/Utils`);
+const Utils= require(`${process.env.PWD}/app/helpers/Utils`);
 
 class Routing {
-  static async apply(server) {
-    var availableRoutes= await this.getAvailableRoutes();
+	
+	static async apply(server) {
+		var availableRoutes= await this.getAvailableRoutes();
 		var availableMiddlewares= await this.getAvailableMiddlewares();
 		var availableControllers= await this.getAvailableControllers();
 
 		this.attachRouteGroups(server, availableRoutes, availableControllers, availableMiddlewares);
 
-	return true;
-  }
+		return true;
+	}
 
-  static getAvailableRoutes() {
-    var routes= new Routes();
+	static getAvailableRoutes() {
+		var routes= new Routes();
+
 		return routes.registeredRouteGroups;
-  }
+	}
 
-  static getAvailableMiddlewares() {
+	static getAvailableMiddlewares() {
 		var middlewares= new Middlewares();
 
 		return middlewares.registeredMiddlewares;
@@ -33,32 +35,39 @@ class Routing {
 		return controllers.registeredControllers
 	}
 
-  static attachRouteGroups(server, routeGroups, controllers, middlewares) {
+	static attachRouteGroups(server, routeGroups, controllers, middlewares) {
 		routeGroups.forEach( (routeGroup, index)=> {
-			var appliedGroupMdlwrs= this.setGroupMiddlewares(routeGroup.middlewares)
+			var appliedGroupMdlwrs= this.setGroupMiddlewares(routeGroup.middlewares, middlewares);
+			
 			this.applyRoutes(server, routeGroup, appliedGroupMdlwrs, controllers, middlewares)
 		})
-
+		
 		return true;
 	}
 
 	static setGroupMiddlewares(groupMiddlewares, availableMiddlewares) {
-		var appliedGroupMdlwrs= [];
+		try {
+			var appliedGroupMdlwrs= [];
 
-		groupMiddlewares.forEach( (middlewares) => {
-			appliedGroupMdlwrs.push(Utils.accessObjectPropertyByString(availableMiddlewares, middlewares))
-		})
+			groupMiddlewares.forEach( (middlewares) => {
+				appliedGroupMdlwrs.push(Utils.accessObjectPropertyByString(availableMiddlewares, middlewares))
+			})
 
-		return appliedGroupMdlwrs;
+			return appliedGroupMdlwrs;
+		}
+		catch(error) {
+			console.error(error);
+			return error;
+		}
 	}
 
-	static applyRoutes(server, routeGroup, appliedGroupMiddlewares, controllers, middlewares) {
+	static async applyRoutes(server, routeGroup, appliedGroupMiddlewares, controllers, middlewares) {
 		try {
-			routeGroup.routes.forEach( (endpoint) => {
-				var method = endpoint[0]
-				, url = endpoint[1]
-				, target = endpoint[2]
-				, routeMiddlewares = ( Array.isArray(endpoint[3]))?endpoint[3]:[]
+			routeGroup.routes.forEach( (route) => {
+				var method = route[0]
+				, url = route[1]
+				, target = route[2]
+				, routeMiddlewares = ( Array.isArray(route[3]))?route[3]:[]
 				, appliedMdlwrs = [];
 
 				routeMiddlewares.forEach( (mdlwr) => {
@@ -67,21 +76,16 @@ class Routing {
 
 				const targetController= Utils.accessObjectPropertyByString(controllers, target);
 
-
-
 				server[method](routeGroup.prefix+url, appliedGroupMiddlewares, appliedMdlwrs, targetController);
 			})
 
-			return true;
+			return true;	
 		}
 		catch(error) {
 			return false;
 		}
 	}
 
-	static attachRouteToServer() {
-
-	}
 }
 
-module.exports = Routing;
+module.exports= Routing;
